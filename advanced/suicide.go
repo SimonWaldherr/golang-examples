dc9f24813e9fd0eda7b153c3dbdcb8093da46c1f
Simon Waldherr <git@simon.waldherr.eu>
2014-03-15 23:52:33 +0100

    suicide app added

---
+++ advanced/suicide.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strconv"
+	"os/exec"
+)
+
+func main() {
+	
+	pid := os.Getpid()
+	str := strconv.Itoa(pid)
+	fmt.Println("Process identifier: ", str)
+	ret, _ := exec.Command("kill", "-9", str).Output()
+	fmt.Println("this will never be printed: ", ret)
+}
