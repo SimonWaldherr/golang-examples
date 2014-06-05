5c0c484e54b6f7def79f5dadf5cbbbe6ebe5b791
Simon Waldherr <git@simon.waldherr.eu>
2014-06-06 00:38:30 +0200

    wait example added

---
+++ advanced/wait.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func f(w *sync.WaitGroup, sec int) {
+	fmt.Println("a")
+	time.Sleep(time.Duration(sec) * time.Second)
+	fmt.Println("b")
+	w.Done()
+}
+
+func main() {
+	fmt.Println("start")
+	var w sync.WaitGroup
+	w.Add(2)
+
+	go func(w *sync.WaitGroup, sec int) {
+		fmt.Println("c")
+		<-time.After(time.Duration(sec) * time.Millisecond)
+		fmt.Println("d")
+		w.Done()
+	}(&w, 2500)
+
+	go f(&w, 4)
+
+	w.Wait()
+	
+	time.Sleep(900000000 * time.Nanosecond)
+	
+	fmt.Println("finish")
+}
--- beginner/input.go
+++ beginner/input.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main() {
-	var i int
+	//arguments
+	if 1 < len(os.Args) {
+		fmt.Print(len(os.Args)-1)
+		fmt.Println(" arguments provided")
+	}
+
+	//Scanf to wait for an input
 	fmt.Println("Enter a number")
+	var i int
 	_, err := fmt.Scanf("%d", &i)
 	if err != nil {
-		fmt.Println(i)
+		fmt.Println("not a number")
+	} else {
+		fmt.Print(i)
+		fmt.Println(" is a number")
+	}
+
+	//Stdin read buffer
+	fmt.Println("Enter a string")
+	bio := bufio.NewReader(os.Stdin)
+	line, _, err := bio.ReadLine()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("you entered \"" + string(line) + "\"")
 	}
 }
