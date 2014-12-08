72a8f81f88ad6febe135bbbc9c3b1523575ba848
Simon Waldherr <git@simon.waldherr.eu>
2014-12-08 02:11:18 +0100

    this program don't wait for a enter-key

---
+++ advanced/getchar.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func readStdin(out chan string, in chan bool) {
+	//no buffering
+	exec.Command("stty", "-f", "/dev/tty", "cbreak", "min", "1").Run()
+	//no visible output
+	//exec.Command("stty", "-f", "/dev/tty", "-echo").Run()
+
+	var b []byte = make([]byte, 1)
+	for {
+		select {
+		case <-in:
+			return
+		default:
+			os.Stdin.Read(b)
+			out <- string(b)
+		}
+	}
+}
+
+func main() {
+	stdin := make(chan string, 1)
+	kill := make(chan bool, 1)
+	var count int = 0
+	var input string
+
+	fmt.Print("this program don't wait for a enter-key\nyou can now start typing\npress \"q\" to quit\n\n")
+
+	go readStdin(stdin, kill)
+	for {
+		str := <-stdin
+
+		if str == "q" {
+			kill <- true
+			close(stdin)
+			break
+		}
+
+		input += str
+		count++
+
+		if count%10 == 0 {
+			fmt.Printf("\n\nyou typed %v keys, quit the demo by pressing \"q\"\n\n", count)
+		}
+	}
+	fmt.Printf("\n\nyou typed %v keys before you quit the program\n", count)
+	fmt.Println(input)
+}
