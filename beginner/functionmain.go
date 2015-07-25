c95a87aabd41466e608159e49940ed9df502b1b3
Simon Waldherr <git@simon.waldherr.eu>
2015-07-25 23:56:32 +0200

    functionmain.go added (main and init function example)

---
+++ beginner/functionmain.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var i int
+
+func init() {
+	fmt.Println("the init function gets started at first")
+}
+
+func main() {
+	fmt.Println("the main function gets called immediately after")
+	if i < 3 {
+		i++
+		fmt.Println("we can also call the main function ourself (but not init)")
+		main()
+	}
+}
