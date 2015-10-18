221065e6dc6269280c27559ca3375102b3765ce9
Simon Waldherr <git@simon.waldherr.eu>
2015-10-18 16:24:01 +0200

    change from "read until \t or til EOF" to "read 65535 bytes or til EOF"

--- expert/tcp.go
+++ expert/tcp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"time"
@@ -10,11 +11,11 @@ import (
 
 const retrytime = 510 * time.Millisecond
 
-func tcpSend(str, addr string) string {
-	return tcpSendHelper(str, addr, 3)
+func tcpClient(str, addr string) string {
+	return tcpClientHelper(str, addr, 3)
 }
 
-func tcpSendHelper(str, addr string, retry int) string {
+func tcpClientHelper(str, addr string, retry int) string {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
 	conn, err := net.DialTCP("tcp4", nil, tcpAddr)
 	if err == nil {
@@ -26,17 +27,21 @@ func tcpSendHelper(str, addr string, retry int) string {
 
 			if retry > 0 {
 				time.Sleep(retrytime)
-				return tcpSendHelper(str, addr, retry-1)
+				return tcpClientHelper(str, addr, retry-1)
 			}
 		}
-		message, _ := bufio.NewReader(conn).ReadString('\t')
-		return fmt.Sprint(message)
+		bytes := make([]byte, 65535)
+		_, err := bufio.NewReader(conn).Read(bytes)
+		if err == nil || err == io.EOF {
+			return string(bytes)
+		}
+		return fmt.Sprint(err)
 	}
 	log.Println(err)
 
 	if retry > 0 {
 		time.Sleep(retrytime)
-		return tcpSendHelper(str, addr, retry-1)
+		return tcpClientHelper(str, addr, retry-1)
 	}
 	return ""
 }
@@ -46,6 +51,6 @@ func main() {
 	var port string = "80"
 	var request string = "GET /index.html"
 
-	response := tcpSend(request, host+":"+port)
-	fmt.Printf("response from %v:%v for the request \"%v\" is: %v\n", host, port, request, response)
+	response := tcpClient(request, host+":"+port)
+	fmt.Printf("response from %v:%v for the request \"%v\" is: \n%v\n", host, port, request, response)
 }
