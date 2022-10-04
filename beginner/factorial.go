0c3c12e9c1840f3f92e098ef9fccceb561378095
Priyanshu Singh <66723839+priyanshusingh9694@users.noreply.github.com>
2022-10-04 17:32:24 +0530

    Create factorial.go

---
+++ beginner/factorial.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func factorial(num int) int{
+	if num == 1 || num == 0{
+		return num
+	}
+	return num * factorial(num-1)
+}
+func main(){
+	fmt.Println(factorial(3))
+}
