9f13116dad58e5fb9ed6fb94e2cc7412b369c2e7
mukul314 <mukulsharma3779@gmail.com>
2022-10-23 12:33:33 +0530

    added bubble sort program

---
+++ advanced/bubble_sort.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func BubbleSort(array[] int)[]int {
+   for i:=0; i< len(array)-1; i++ {
+      for j:=0; j < len(array)-i-1; j++ {
+         if (array[j] > array[j+1]) {
+            array[j], array[j+1] = array[j+1], array[j]
+         }
+      }
+   }
+   return array
+}
+func main() {
+   array:= []int{11, 14, 3, 8, 18, 17, 43};
+   fmt.Println(BubbleSort(array))
+}
