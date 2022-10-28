26f7a0414533834659b3e50748eccf2e307ad0cb
Rebin Qadir <4rebin@gmail.com>
2022-10-28 21:09:14 +0300

    Sum numbers of array using Channel in golang (#85)
    * Sum numers of array using Channel in golang
    * Update Channel-2.go
    
    Co-authored-by: Simon Waldherr <git@simon.waldherr.eu>

---
+++ advanced/Channel-2.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbersArray := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	channel := make(chan int)
+
+	go sumNumbers(numbersArray[:len(numbersArray)/2], channel)
+	go sumNumbers(numbersArray[len(numbersArray)/2:], channel)
+
+	number1 := <-channel
+	number2 := <-channel
+
+	fmt.Println("Sum of two numbers in first goroutine: ", number1)
+	fmt.Println("Sum of two numbers in second goroutine: ", number2)
+
+	fmt.Println("Total : ", number1+number2)
+}
+
+func sumNumbers(numbers []int, channel chan int) {
+	result := 0
+	for _, value := range numbers {
+		result += value
+	}
+	channel <- result
+}
