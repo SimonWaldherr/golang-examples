c9ac89884ea8996d15949d2a0c604f6704111c44
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-10-01 08:46:48 +0530

    resolved the issues

--- beginner/MongoDB/database.go
+++ beginner/MongoDB/database.go
@@ -13,96 +13,95 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-// Movies represents the structure of a movie document in the MongoDB collection.
-// The struct tags `json` and `bson` ensure proper serialization for both JSON and MongoDB BSON formats.
 type Movies struct {
-	ID         string `json:"id" bson:"_id,omitempty"` // ID field, auto-handled by MongoDB (_id)
-	Name       string `json:"name" bson:"name"`        // Name of the movie
-	Release    int    `json:"date" bson:"date"`        // Release year of the movie
-	Collection string `json:"money" bson:"collection"` // Box office collection
+	ID         string `json:"id" bson:"_id,omitempty"`
+	Name       string `json:"name" bson:"name"`
+	Release    int    `json:"date" bson:"date"`
+	Collection string `json:"money" bson:"collection"`
 }
 
-// moviesCollection will hold the MongoDB collection object for movies.
-// This will be initialized after a successful connection to the database.
 var moviesCollection *mongo.Collection
 
-// connectToMongo initializes and connects to the MongoDB instance.
-// It returns a MongoDB client object if successful, or an error otherwise.
-func connectToMongo() (*mongo.Client, error) {
-	// Define options for the MongoDB client, including the connection URI.
-	clientOptions := options.Client().ApplyURI("your_connection_string_here")
-
-	// Create a new MongoDB client using the options defined above.
+// Separate function for creating MongoDB client
+func createMongoClient(uri string) (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
-		return nil, err // Return the error if client creation fails
+		return nil, err
 	}
-
-	// Create a context with a 10-second timeout for MongoDB operations.
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel() // Ensure the context is canceled after the operation completes
-
-	// Connect the client to the MongoDB server.
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, err // Return the error if the connection fails
-	}
-
-	// Ping the MongoDB server to ensure the connection is alive.
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		return nil, err // Return the error if the ping fails
-	}
-
-	fmt.Println("Connected to MongoDB!") // Print a success message on successful connection
-	return client, nil                   // Return the MongoDB client
+	return client, nil
 }
 
-// GetMovies is a handler function for the "/movies" endpoint.
-// It retrieves all movies from the MongoDB collection and sends them as a JSON response.
-func GetMovies(c *gin.Context) {
-	// Create a new context with a 10-second timeout to limit the duration of database operations.
+// Separate function for connecting to MongoDB
+func connectToMongo(client *mongo.Client) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel() // Ensure the context is canceled after the operation completes
+	defer cancel()
+	return client.Connect(ctx)
+}
 
-	// Find all movie documents in the moviesCollection. bson.M{} represents an empty filter (no filtering).
-	cursor, err := moviesCollection.Find(ctx, bson.M{})
+// Function to test connection with MongoDB
+func pingMongo(client *mongo.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return client.Ping(ctx, readpref.Primary())
+}
+
+// Function to initialize MongoDB collection
+func initializeMongoCollection(client *mongo.Client, dbName, collectionName string) {
+	moviesCollection = client.Database(dbName).Collection(collectionName)
+}
+
+// Handler function to get movies from MongoDB
+func GetMovies(c *gin.Context) {
+	movies, err := fetchMoviesFromDB()
 	if err != nil {
-		// If there is an error during the find operation, return a 500 status code and an error message.
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "cannot retrieve movies"})
 		return
 	}
-	defer cursor.Close(ctx) // Close the cursor after we're done processing the results
-
-	// Define a slice to hold the list of movies.
-	var movies []Movies
-
-	// Decode all the documents from the cursor into the `movies` slice.
-	if err = cursor.All(ctx, &movies); err != nil {
-		// If decoding fails, return a 500 status code and an error message.
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not decode movies"})
-		return
-	}
-
-	// Send the movies as a JSON response with a 200 status code.
 	c.IndentedJSON(http.StatusOK, movies)
 }
 
-// main is the entry point of the application.
+// Fetch movies from the database
+func fetchMoviesFromDB() ([]Movies, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := moviesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var movies []Movies
+	if err = cursor.All(ctx, &movies); err != nil {
+		return nil, err
+	}
+
+	return movies, nil
+}
+
 func main() {
-	// Initialize a new Gin router instance.
 	router := gin.Default()
 
-	// Connect to MongoDB using the connectToMongo function.
-	client, err := connectToMongo()
+	client, err := createMongoClient("your_connection_string_here")
 	if err != nil {
-		// If connection to MongoDB fails, panic and stop the application.
 		panic(err)
 	}
-	defer client.Disconnect(context.TODO()) // Ensure MongoDB client disconnects when the application exits
 
-	//here in this Set the global moviesCollection to point to the "Movies" collection inside the "Films" database.
-	moviesCollection = client.Database("name_of_the_database").Collection("name_of_the_collection")
+	err = connectToMongo(client)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	err = pingMongo(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	initializeMongoCollection(client, "name_of_the_database", "name_of_the_collection")
+
 	router.GET("/movies", GetMovies)
-	router.Run("use_your_local_host") // use your local host
+	router.Run("use_your_local_host")
 }
